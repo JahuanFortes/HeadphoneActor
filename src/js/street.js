c78d4ed6896423d64c9bc298a01c58cf551c1790
JahuanFortes <marleyjahuan@gmail.com>
2023-06-05 16:53:54 +0200

    build fix

--- src/js/street.js
+++ src/js/street.js
@@ -15,18 +15,18 @@ export class Street extends Actor {
     super();
     const streetAnimation = SpriteSheet.fromImageSource({
       image: Resources.BgImg,
-      grid: { rows: 1, colums: 1, spritewidth: 1600, spriteheight: 600 },
+      grid: { rows: 1, columns: 4, spritewidth: 800, spriteheight: 600 },
     });
     const showAnimation = Animation.fromSpriteSheet(
       streetAnimation,
-      range(1, 1),
+      range(1, 4),
       80
     );
 
     this.graphics.add("showAnimation", showAnimation);
     this.graphics.use(showAnimation);
-    console.log(showAnimation);
   }
+
   onInitialize(engine) {
     const streetIMG = Resources.StreetIMG.toSprite();
     this.offset = streetIMG.width;
@@ -44,11 +44,12 @@ export class Street extends Actor {
       ],
     });
     this.graphics.anchor = new Vector(0, 0);
-    // this.graphics.add(groupstreetIMG);
+    this.graphics.add(groupstreetIMG);
     this.pos = new Vector(0, 180);
     this.vel = new Vector(-200, 0);
     this.scale = new Vector(0.9, 0.9);
   }
+
   onPostUpdate(engine, delta) {
     if (this.pos.x < -this.offset * 1) {
       this.pos = new Vector(0, 180);
